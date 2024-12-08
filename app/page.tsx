130c6b5e6230a7e4e642ae3b91d6ef84bfa6b2d9
Akseli Palmer <akseli@toddsden.com>
2024-12-08 17:31:24 -0500

    boiler plate for app

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read our docs real estate game
           </a>
         </div>
       </main>
